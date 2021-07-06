6e28a271f6571449530d6adf4f9f6e2d3033784b
naresh128 <85680658+naresh128@users.noreply.github.com>
2021-07-06 19:49:41 +0530

    Add files via upload
    Added Files

---
+++ GitRestProject/src/main/java/com/test/GitRestProject/GitRestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.GitRestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRestProjectApplication.class, args);
+	}
+
+}
---
+++ GitRestProject/src/test/java/com/test/GitRestProject/GitRestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.GitRestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitRestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
